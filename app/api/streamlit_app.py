c7165ba48559a6df3f03651e90a9342a816bb789
Miles Lilly <mileslilly2@gmail.com>
2025-04-07 21:22:46 -0400

    ppp

--- app/api/streamlit_app.py
+++ app/api/streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 from app.core.search import search_faiss_index
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
 
 st.set_page_config(page_title="WVU Semantic Search", page_icon="🔍", layout="wide")
 

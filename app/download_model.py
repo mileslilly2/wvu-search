67b65fe0ead7dbe0756bc38adf099b4f96f769b6
Miles Lilly <mileslilly2@gmail.com>
2025-04-07 23:52:27 -0400

    download_model fix

--- app/download_model.py
+++
@@ -1,8 +0,0 @@
-# download_model.py
-from sentence_transformers import SentenceTransformer
-
-model_name = "all-MiniLM-L6-v2"
-model = SentenceTransformer(model_name)
-model.save("models/all-MiniLM-L6-v2")
-
-print("✅ Model downloaded and saved to models/all-MiniLM-L6-v2/")
